3639a7462a9bf7be0f3b8735f6e063bc7256d793
John <johnsmilga@gmail.com>
2019-03-25 18:11:56 -0700

    longest words math sequences

---
+++ challenges/1-math-sequences.js
@@ -0,0 +1,23 @@
+function ArithGeom(arr) {
+  let arith = new Set();
+  let geo = new Set();
+
+  for (let i = 1; i < arr.length; i++) {
+    let number1 = arr[i] - arr[i - 1];
+    arith.add(number1);
+    let number2 = arr[i] / arr[i - 1];
+    geo.add(number2);
+  }
+  if (arith.size === 1) {
+    return "Arithmetic";
+  }
+  if (geo.size === 1) {
+    return "Geometric";
+  }
+
+  return -1;
+}
+
+console.log(ArithGeom([2, 4, 6]));
+console.log(ArithGeom([3, 9, 27]));
+console.log(ArithGeom([2, 4, 8, 45]));
